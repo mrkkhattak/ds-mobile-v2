48b5d9299d30d71ae40a606ae195358e72aa5115
MoizDoerz <moiz@doerz.tech>
2025-11-07 03:12:12 +0500

    i have created the homeScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,116 @@
+import MainLayout from "@/components/layout/MainLayout";
+import { MainButton } from "@/components/ui/Buttons";
+import { MainHeading, SecondryHeading } from "@/components/ui/Heading";
 import { useAuthStore } from "@/store/authstore";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import LibrarIcon from "../../assets/images/icons/Group 1.svg";
+import StreakIcon from "../../assets/images/icons/Streak.svg";
 
 const index = () => {
   const { signOut } = useAuthStore();
   const user = useAuthStore((s) => s.user);
   console.log("user", user);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <TouchableOpacity
-        onPress={() => {
-          signOut();
+    <MainLayout>
+      <View style={{ flexGrow: 1 }}>
+        <View style={{ marginTop: 60, paddingHorizontal: 40 }}></View>
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <View style={{ marginHorizontal: 20, marginTop: 40 }}>
+            <MainHeading style={{ textAlign: "left" }}>
+              Daily Spruce
+            </MainHeading>
+          </View>
+          <SecondryHeading
+            style={{
+              textAlign: "left",
+              color: "white",
+              fontWeight: "300",
+              fontFamily: "inter",
+              fontSize: 14,
+              marginHorizontal: 20,
+            }}
+          >
+            SMALL STEPS. BIG IMPACT!
+          </SecondryHeading>
+          <View
+            style={{
+              justifyContent: "flex-end",
+              alignItems: "center",
+              gap: 10,
+            }}
+          >
+            <StreakIcon />
+            <MainButton
+              onPress={() => {}}
+              label="Slide to start sprucing"
+              style={{ width: "90%", marginBottom: 10 }}
+            />
+          </View>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: "white",
+          borderTopRightRadius: 40,
+          borderTopLeftRadius: 40,
         }}
       >
-        <Text>LOGOUT</Text>
-      </TouchableOpacity>
-    </View>
+        <SecondryHeading style={{ marginLeft: 40, textAlign: "left" }}>
+          Today
+        </SecondryHeading>
+        <View
+          style={{
+            paddingHorizontal: 40,
+            marginTop: 20,
+          }}
+        >
+          <MainHeading
+            style={{
+              color: "#6915E0",
+              fontWeight: "700",
+              fontSize: 20,
+              lineHeight: 36,
+              fontFamily: "inter",
+            }}
+          >
+            Need a head start?
+          </MainHeading>
+          <SecondryHeading
+            style={{
+              fontSize: 16,
+              lineHeight: 22,
+              fontWeight: "300",
+              fontFamily: "inter",
+            }}
+          >
+            Open your Task Library to create your list in seconds and start
+            sprucing without overthinking
+          </SecondryHeading>
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <LibrarIcon />
+          </View>
+          {/* <MainButton
+            onPress={() => {
+              signOut();
+            }}
+            label="LOGOUT"
+          /> */}
+        </View>
+        {/* <TouchableOpacity
+          onPress={() => {
+            signOut();
+          }}
+        >
+          <Text>LOGOUT</Text>
+        </TouchableOpacity> */}
+      </View>
+    </MainLayout>
   );
 };
 
--- app/navigation/TabNavigator.tsx
+++ app/navigation/TabNavigator.tsx
@@ -1,15 +1,91 @@
 // navigation/TabNavigator.tsx
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Text, View } from "react-native";
 import Settings from "../(tabs)/explore";
 import Home from "../(tabs)/index";
+import Icon2 from "../../assets/images/icons/Shuffle.svg";
+
+import Icon3 from "../../assets/images/icons/Shuffle (1).svg";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Settings" component={Settings} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "transparent",
+          position: "absolute",
+          elevation: 0,
+          borderTopWidth: 0,
+          marginBottom: 20,
+        },
+        tabBarShowLabel: false, // we'll use a custom label below
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+
+                width: 200,
+              }}
+            >
+              <Icon3
+                width={200}
+                height={100}
+                fill={focused ? "#8DE016" : "#AAA"}
+              />
+              <Text
+                style={{
+                  color: focused ? "#8DE016" : "#AAA",
+                  fontSize: 15,
+                  marginTop: 4,
+                  fontWeight: "600",
+                }}
+              >
+                Home
+              </Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                width: 200,
+              }}
+            >
+              <Icon2
+                width={200}
+                height={100}
+                fill={focused ? "#8DE016" : "#AAA"}
+              />
+              <Text
+                style={{
+                  color: focused ? "#8DE016" : "#575261",
+                  fontSize: 15,
+                  marginTop: 4,
+                  fontWeight: "600",
+                }}
+              >
+                Settings
+              </Text>
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
